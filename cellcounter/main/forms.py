c784af03850992c689e449f71cfd5470182448cd
JV Clemence <james+github@jvc26.org>
2012-09-30 20:38:18 +0100

    Implement forms for ErythropoiesisFindings, GranulopoiesisFindings, MegakaryocyteFeatures and CellCount

--- cellcounter/main/forms.py
+++ cellcounter/main/forms.py
@@ -1,6 +1,6 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, HiddenInput
 
-from cellcounter.main.models import CellCountInstance, BoneMarrowBackground
+from cellcounter.main.models import CellCount, CellCountInstance, BoneMarrowBackground, ErythropoiesisFindings, GranulopoiesisFindings, MegakaryocyteFeatures
 
 class CellCountInstanceForm(ModelForm):
     class Meta:
@@ -11,3 +11,29 @@ class BoneMarrowBackgroundForm(ModelForm):
     class Meta:
         model = BoneMarrowBackground
         exclude = ('cell_count_instance',) 
+
+class ErythropoiesisFindingsForm(ModelForm):
+    class Meta:
+        model = ErythropoiesisFindings
+        exclude = ('cell_count_instance',) 
+
+class GranulopoiesisFindingsForm(ModelForm):
+    class Meta:
+        model = GranulopoiesisFindings
+        exclude = ('cell_count_instance',) 
+
+class MegakaryocyteFeaturesForm(ModelForm):
+    class Meta:
+        model = MegakaryocyteFeatures
+        exclude = ('cell_count_instance',) 
+
+class CellCountForm(ModelForm):
+    auto_id = False
+
+    class Meta:
+        model = CellCount
+        widgets = {
+                'cell': HiddenInput(),
+                'normal_count': HiddenInput,
+                'abnormal_count': HiddenInput}
+        exclude = ('cell_count_instance', 'comment',)
