ccbbdcd8474321e82b5f8435f3456023f0a6de70
Kristian Glass <git@doismellburning.co.uk>
2012-10-04 22:07:10 +0100

    gunicorn

--- cellcounter/settings.py
+++ cellcounter/settings.py
@@ -115,6 +115,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'gunicorn',
     'cellcounter.main',
 )
 
---
+++ gunicorn_conf.py
@@ -0,0 +1,3 @@
+accesslog = '-'
+access_log_format = '%({Host}i)s %(h)s %(l)s "%({X-Remote-User-Id}o)s: %({X-Remote-User-Name}o)s" %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s'
+workers = 3
