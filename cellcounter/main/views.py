3a22f5e3e60b9115429a6fa060ed0be11815242e
JV Clemence <james+github@jvc26.org>
2012-10-11 12:35:24 +0100

    Implement view_count view, and supporting tests

--- cellcounter/main/views.py
+++ cellcounter/main/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render_to_response, HttpResponseRedirect
+from django.http import HttpResponseForbidden
+from django.shortcuts import render_to_response, HttpResponseRedirect, get_object_or_404
 from django.template import RequestContext
+from django.contrib.auth.decorators import login_required
 
 from cellcounter.main.forms import CellCountInstanceForm, BoneMarrowBackgroundForm, CellCountForm, GranulopoiesisFindingsForm, ErythropoiesisFindingsForm, MegakaryocyteFeaturesForm
 
-from cellcounter.main.models import BoneMarrowBackground, CellCount, CellType
+from cellcounter.main.models import BoneMarrowBackground, CellCount, CellType, CellCountInstance
 from cellcounter.main.utils import get_cellcount_formset, get_celltype_list
 
 def new_count(request):
@@ -71,8 +73,28 @@ def new_count(request):
              'cellcountformset': cellcount_formset,},
             context_instance=RequestContext(request))
 
-def view_count(request):
-    pass
+@login_required
+def view_count(request, count_id):
+    cell_count = get_object_or_404(CellCountInstance, pk=count_id)
+
+    if request.user != cell_count.user:
+        return HttpResponseForbidden()
+
+    cellcount_list = cell_count.cellcount_set.all()
+
+    count_total = 0
+    for count in cellcount_list:
+        count_total = count_total + count.normal_count
+        count_total = count_total + count.abnormal_count
+
+    return render_to_response('main/report.html',
+            {'cellcount': cell_count,
+             'bonemarrowbackground': cell_count.bonemarrowbackground,
+             'erythropoiesis': cell_count.erythropoiesisfindings,
+             'granulopoiesis': cell_count.granulopoiesisfindings,
+             'megakaryocytes': cell_count.megakaryocytefeatures,
+             'cellcount_list': cell_count.cellcount_set.all()},
+            context_instance=RequestContext(request))
 
 def edit_count(request):
     pass
