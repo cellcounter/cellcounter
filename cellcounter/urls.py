911e8f142f48b6dc9d31b80dc43151a0b653f742
JV Clemence <james+github@jvc26.org>
2012-10-24 18:18:24 +0100

    Enable user profile pages

--- cellcounter/urls.py
+++ cellcounter/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
-from cellcounter.main.views import new_count, view_count, edit_count, ListMyCountsView
+from cellcounter.main.views import new_count, view_count, edit_count, ListMyCountsView, UserDetailView
 
 admin.autodiscover()
 
@@ -19,7 +19,8 @@ urlpatterns = patterns('',
     url(r'^count/(?P<count_id>\d+)/$', view_count, name="view_count"),
     url(r'^count/(?P<count_id>\d+)/edit/$', edit_count, name="edit_count"),
 
-    url(r'^user/$', ListMyCountsView.as_view(), name="my_counts"),
+    url(r'^user/(?P<pk>\d+)/$', UserDetailView.as_view(), name="user_home"),
+    url(r'^user/(?P<pk>\d+)/counts/$', ListMyCountsView.as_view(), name="my_counts"),
 
     url(r'^login/$', login, {'template_name': 'main/login.html'}, name='login'),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
