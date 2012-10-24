150058856f10992d0f65f47d79ac14e2f52818cc
JV Clemence <james+github@jvc26.org>
2012-10-24 12:10:10 +0100

    Add URL to enable user list view

--- cellcounter/urls.py
+++ cellcounter/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
 
-from cellcounter.main.views import new_count, view_count, edit_count
+from cellcounter.main.views import new_count, view_count, edit_count, ListMyCountsView
 
 admin.autodiscover()
 
@@ -18,6 +18,8 @@ urlpatterns = patterns('',
     url(r'^count/new/$', new_count, name="new_count"),
     url(r'^count/(?P<count_id>\d+)/$', view_count, name="view_count"),
     url(r'^count/(?P<count_id>\d+)/edit/$', edit_count, name="edit_count"),
+
+    url(r'^user/$', ListMyCountsView.as_view()),
 
     url(r'^login/$', login, {'template_name': 'main/login.html'}, name='login'),
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
