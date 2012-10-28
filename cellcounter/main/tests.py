d23f20ed24ce15acb5745d6be5954abbadfec894
JV Clemence <james+github@jvc26.org>
2012-10-28 16:23:27 +0000

    Tests should test rounded M:E ratio

--- cellcounter/main/tests.py
+++ cellcounter/main/tests.py
@@ -223,7 +223,7 @@ class TestCellCountInstance(TestCase):
 
     def test_me_ratio(self):
         cellcount = CellCountInstance.objects.get(id=1)
-        self.assertEqual(19.833333333333332, cellcount.myeloid_erythroid_ratio())
+        self.assertEqual(19.83, cellcount.myeloid_erythroid_ratio())
 
     def test_me_ratio_erythroids_0(self):
         cellcount = CellCountInstance.objects.get(id=2)
