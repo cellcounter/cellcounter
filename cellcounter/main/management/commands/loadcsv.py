f0d47bd69eafab32c47332cee56483e131c10679
martin <martin@abc.(none)>
2013-01-26 17:58:39 +0000

    Load images from csv, display a pade of images

---
+++ cellcounter/main/management/commands/loadcsv.py
@@ -0,0 +1,21 @@
+from django.core.management.base import BaseCommand, CommandError
+from cellcounter.main.models import CellImage, SimilarLookingGroup, CellType
+import csv
+
+class dialect(csv.Dialect):
+    pass
+
+class Command(BaseCommand):
+    args = '<csvfile1 csvfile2 ...>'
+    help = 'Loads images and descriptions from specified csv file(s)'
+
+    def handle(self, *args, **options):
+         for filename in args:
+             file_ = csv.DictReader(open(filename), dialect="excel-tab")
+             for line in file_:
+                 celltype = CellType.objects.get(readable_name = line["CellType"])
+                 ci = CellImage(title = line["Title"],
+                                description = line["Description"],
+                                file = line["Filename"],
+                                celltype = celltype)
+                 ci.save() 
