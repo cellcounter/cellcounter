8124346441f774a40707041eac7fd5376109899e
Oliver Madge <oghm2-github@srcf.net>
2013-01-26 17:58:47 +0000

    Added visualisation.

--- cellcounter/main/static/js/visualise.js
+++ cellcounter/main/static/js/visualise.js
@@ -1,29 +1,35 @@
 
-$(document).ready(function() {
+//$(document).ready(function() {
+function init_visualisation() {
     "use strict";
 
     // Size of doughnut.
-    var size = 200;
+    //var size = 200;
 
     // Set up a function used to calculate the angles of the doughnut.
-    var pie = d3.layout.pie()
+    pie = d3.layout.pie()
         // This ensures the segments remain ordered.
         .sort(null) 
         .value(function(d) { return d.count; });
 
     // Set up a function used to calculate the width of the doughnut.
-    var arc = d3.svg.arc()
+    arc = d3.svg.arc()
         .outerRadius(size / 2)
         .innerRadius(size / 4);
 
     // Set up the SVG element for the doughnut on the page.
-    var doughnut = d3.select('#doughnut').append('svg')
+    doughnut = d3.select('#doughnut').append('svg')
         .attr('width', size)
         .attr('height', size)
       .append('g')
         // This ensures the doughnut is centred in the SVG element.
         .attr('transform', 'translate(' + size / 2 + ',' + size / 2 + ')');
 
+    data = [];
+
+    for(var x in counters) {
+        data.push(counters[x]);
+    }
     // Bind the data to the doughnut and create a grouping element ('g') for
     // each item of data.
     doughnut.selectAll('g')
@@ -39,5 +45,28 @@ $(document).ready(function() {
         .attr('dy', '.35em')
         .style('text-anchor', 'middle');
  
-});
+}
+
+function update_visualisation() {
+
+    data = [];
+
+    for(var x in counters) {
+        data.push(counters[x]);
+    }
+
+    // Update the doughnut's data.
+    doughnut.selectAll('g')
+        .data(pie(data));
+
+    // Redraw the paths.
+    doughnut.selectAll('g').select('path')
+        .attr('d', arc);
+
+    // Display the text and put it in the right location.
+    doughnut.selectAll('g').select('text')
+        .attr('transform', function(d) { return 'translate(' + arc.centroid(d) + ')'; })
+        .text(function(d) { return d.value > 0 ? d.data.key : ''; });
+
+}
 
