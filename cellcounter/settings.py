8522ab428a3b8e54c3b309d5e83121850029c75f
Craig Loftus <craigloftus@gmail.com>
2013-01-27 09:55:00 +0000

    Adding loading of default keyboard for new users

--- cellcounter/settings.py
+++ cellcounter/settings.py
@@ -17,6 +17,8 @@ DATABASES = {'default': {
 	}
 }
 
+PROJECT_DIR = os.path.dirname(__file__)
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
