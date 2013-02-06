2d5c5de8450da38010f5b17825d9f2a20f643c45
Oliver Madge <oghm2-github@srcf.net>
2013-02-06 16:28:58 +0000

    Default to https if not in debug mode

--- cellcounter/settings.py
+++ cellcounter/settings.py
@@ -92,6 +92,16 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cellcounter.middleware.SecureRequiredMiddleware',
+)
+
+HTTPS_SUPPORT = True
+
+SECURE_REQUIRED_PATHS = (
+    '/admin/',
+    '/count/',
+    '/login/',
+    '/accounts/',
 )
 
 ROOT_URLCONF = 'cellcounter.urls'
