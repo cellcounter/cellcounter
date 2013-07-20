982b8570ebe17530d60f35152c4ea31a0e085403
JV Clemence <james+github@jvc26.org>
2013-07-20 16:28:31 +0100

    Remove accounts app

--- cellcounter/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns
-
-from cellcounter.accounts.views import KeyboardLayoutView
-
-urlpatterns = patterns('',
-    (r'^keyboard/$', KeyboardLayoutView.as_view()),
-)
