a8bb4c40f85054bce1d150b29413baf897834215
JV Clemence <james+github@jvc26.org>
2013-10-25 15:28:20 +0100

    Remove old v1 keyboardapi

--- cellcounter/keyboardapi/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-
-from cellcounter.keyboardapi.views import KeyboardAPIView
-
-urlpatterns = patterns('',
-    url(r'^$', KeyboardAPIView.as_view(), name="base_keyboard"),
-    url(r'^(?P<pk>\d+)/$', KeyboardAPIView.as_view(), name="select_keyboard"),
-)
