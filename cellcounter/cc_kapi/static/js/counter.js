38d8e0e34fb20b25da1373b0ef45b7ba1caa10d3
JV Clemence <james+github@jvc26.org>
2014-02-04 15:22:15 +0000

    Additional change to ensure abnormal count is in red in both results table and in keyboard count. Brackets currently retained.

--- cellcounter/cc_kapi/static/js/counter.js
+++ cellcounter/cc_kapi/static/js/counter.js
@@ -454,7 +454,7 @@ function update_keyboard() {
                 var name = cell_data.abbr;
 
                 item.append("<div class=\"name\">"+name+"</div>");
-                item.append("<div class=\"count\"><span class=\"countval\">"+cell_types[cell_id].count+"</span> (<span class=\"abnormal\">"+cell_types[cell_id].abnormal+"</span>)</div>");
+                item.append("<div class=\"count\"><span class=\"countval\">"+cell_types[cell_id].count+"</span> (<span class=\"abnormal abnormal_count\">"+cell_types[cell_id].abnormal+"</span>)</div>");
 
                 // Attach cell colour to key
                 item.find("p").css("background-color", cell_data.colour);
