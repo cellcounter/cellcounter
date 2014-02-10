f7a4e00deaa4bea9dd733ab851122047149dfea4
JV Clemence <james+github@jvc26.org>
2014-02-10 10:31:37 +0000

    Delegate accounts related URLs to accounts app

--- cellcounter/urls.py
+++ cellcounter/urls.py
@@ -32,6 +32,7 @@ urlpatterns = patterns('',
     url(r'^logout/$', logout, {'next_page': '/'}, name='logout'),
 
     url(r'^api/keyboard/', include('cellcounter.cc_kapi.urls')),
+    url(r'^accounts/', include('cellcounter.accounts.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^terms$', TemplateView.as_view(template_name="main/terms.html"),
