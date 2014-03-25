f8ff6606d615f8290ec4df1475373c5434771040
JV Clemence <james+github@jvc26.org>
2014-03-25 09:01:43 +0000

    Add modelserializer for CellType model

--- cellcounter/main/serializers.py
+++ cellcounter/main/serializers.py
@@ -1,1 +1,8 @@
-__author__ = 'jvc26'
+from rest_framework import serializers
+
+from .models import CellType
+
+
+class CellTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CellType
