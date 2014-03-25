39433a79790943a6f8395065621cb32d905cf9fa
JV Clemence <james+github@jvc26.org>
2014-03-25 16:00:14 +0000

    Basic testing for cellcounter.main, fixes broken context initiation

--- cellcounter/main/tests.py
+++ cellcounter/main/tests.py
@@ -3,6 +3,8 @@ import factory
 
 from django.contrib.auth.models import User
 from django.utils.timezone import utc
+from django.core.urlresolvers import reverse
+from django_webtest import WebTest
 
 from cellcounter.main.models import *
 
@@ -17,7 +19,11 @@ CELLTYPE_LIST = [('Neutrophils', 'neutrophils'),
                  ('Lymphocytes', 'lymphocytes'),
                  ('Monocytes', 'monocytes'),
                  ('Plasma cells', 'plasma_cells'),
-                 ('Other', 'other'),]
+                 ('Lymphoblasts', 'ly_blasts'),
+                 ('Other', 'other')]
+
+CELLTYPE_LIST = '[{"id": 1, "readable_name": "test", "machine_name": "test", "abbr_name": "test", "comment": "Test", "visualisation_colour": "#FFFFFF"}]'
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = User
@@ -33,11 +39,33 @@ class UserFactory(factory.django.DjangoModelFactory):
     last_login = datetime.datetime.utcnow().replace(tzinfo=utc)
     date_joined = datetime.datetime.utcnow().replace(tzinfo=utc)
 
+
 class CellTypeFactory(factory.django.DjangoModelFactory):
     FACTORY_FOR = CellType
     
-    readable_name = '' 
-    machine_name = '' 
-    abbr_name = ''
-    comment = ""
-    visualisaion_colour = ''
+    readable_name = 'test'
+    machine_name = 'test'
+    abbr_name = 'test'
+    comment = "Test"
+    visualisation_colour = '#FFFFFF'
+
+
+class TestMainViews(WebTest):
+    def setUp(self):
+        self.user = UserFactory()
+        self.cell_type = CellTypeFactory()
+
+    def test_new_count_view(self):
+        response = self.app.get(reverse('new_count'))
+        self.assertFalse(response.context['logged_in'])
+        self.assertEqual(200, response.status_code)
+
+    def test_new_count_login(self):
+        response = self.app.get(reverse('new_count'), user=self.user)
+        self.assertTrue(response.context['logged_in'])
+        self.assertEqual(200, response.status_code)
+
+    def test_get_celltype_api(self):
+        response = self.app.get(reverse('cell_types'))
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(CELLTYPE_LIST, response.body)
