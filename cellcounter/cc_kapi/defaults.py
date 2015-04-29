13f5635e13464ea3f4ce6dda6735d7f0dedb0c28
JV Clemence <james+github@jvc26.org>
2015-04-29 21:50:50 +0100

    djangorestframework now outputs without spaces by default

--- cellcounter/cc_kapi/defaults.py
+++ cellcounter/cc_kapi/defaults.py
@@ -43,4 +43,4 @@ MOCK_KEYBOARD = {"mappings": [{"cellid": 1, "key": "q"},
                  "last_modified": "2013-10-22T12:15:13.201Z",
                  "user": None}
 
-DEFAULT_KEYBOARD_STRING = """{"mappings": [{"cellid": 1, "key": "q"}, {"cellid": 2, "key": "w"}, {"cellid": 3, "key": "e"}, {"cellid": 4, "key": "r"}, {"cellid": 5, "key": "t"}, {"cellid": 8, "key": "a"}, {"cellid": 9, "key": "s"}, {"cellid": 11, "key": "d"}, {"cellid": 10, "key": "f"}, {"cellid": 13, "key": "g"}, {"cellid": 7, "key": "z"}, {"cellid": 6, "key": "x"}, {"cellid": 12, "key": "c"}], "created": "2013-10-22T12:15:05.118Z", "is_primary": true, "label": "Default", "last_modified": "2013-10-22T12:15:13.201Z", "user": null}"""
+DEFAULT_KEYBOARD_STRING = """{"mappings":[{"cellid":1,"key":"q"},{"cellid":2,"key":"w"},{"cellid":3,"key":"e"},{"cellid":4,"key":"r"},{"cellid":5,"key":"t"},{"cellid":8,"key":"a"},{"cellid":9,"key":"s"},{"cellid":11,"key":"d"},{"cellid":10,"key":"f"},{"cellid":13,"key":"g"},{"cellid":7,"key":"z"},{"cellid":6,"key":"x"},{"cellid":12,"key":"c"}],"created":"2013-10-22T12:15:05.118Z","is_primary":true,"label":"Default","last_modified":"2013-10-22T12:15:13.201Z","user":null}"""
