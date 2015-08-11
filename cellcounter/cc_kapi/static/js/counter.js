188e4476e0a7a540dbd9461b6877622e53c9afef
JV Clemence <james+github@jvc26.org>
2015-08-11 15:09:18 +0100

    Log count stat on keyboard resetting

--- cellcounter/cc_kapi/static/js/counter.js
+++ cellcounter/cc_kapi/static/js/counter.js
@@ -123,15 +123,7 @@ $(document).ready(function() {
             }
 
             if (total > 0) {
-                if (total > 75) {
-                    $.ajax({
-                        url: '/api/stats/',
-                        type: 'POST',
-                        data: JSON.stringify({"count_total": total}),
-                        contentType: "application/json; charset=utf-8",
-                        async: true
-                    });
-                }
+                log_stats(total);
                 display_stats(total);
             }
 
@@ -439,6 +431,12 @@ function register_resets() {
         $("#confirm-reset").modal("show");
     });
     $('#reset-count').on('click', function() {
+        var total = 0;
+        for (var i=0; i < count_data.length; i++) {
+            total += count_data[i].count;
+            total += count_data[i].abnormal;
+        }
+        log_stats(total);
         reset_counters();
         $("#confirm-reset").modal("hide");
     });
@@ -563,6 +561,19 @@ function display_stats(total, format) {
     }
 
     chart2.render();
+}
+
+function log_stats(total) {
+    use strict;
+    if (total > 75) {
+        $.ajax({
+            url: '/api/stats/',
+            type: 'POST',
+            data: JSON.stringify({"count_total": total}),
+            contentType: "application/json; charset=utf-8",
+            async: true
+        });
+    }
 }
 
 function set_keyboard(mapping) {
