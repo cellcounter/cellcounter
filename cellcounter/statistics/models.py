cd2c74b1c23eb11d68350a89bd34b1383a93d9b6
JV Clemence <james+github@jvc26.org>
2015-09-10 09:29:33 +0100

    Added new model for managing stats collection

--- cellcounter/statistics/models.py
+++ cellcounter/statistics/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class CountInstance(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
+    session_id = models.CharField(max_length=32)
+    ip_address = models.GenericIPAddressField()
+    user = models.ForeignKey(User, null=True, default=None)
     count_total = models.IntegerField()
