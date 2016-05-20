7f72a0f1ee9332bcf455ca2c9f85186fa94dc066
Olly Madge <oghm2@srcf.ucam.org>
2016-05-20 20:37:14 +0100

    Fixed some warnings about deprecated url config.

--- cellcounter/urls.py
+++ cellcounter/urls.py
@@ -5,25 +5,55 @@ from django.contrib import admin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.views.generic import TemplateView
 
-from cellcounter.main import views
+from django.contrib import admin
+from django.contrib.auth.views import login, logout
+
+from django.conf.urls.static import static
+
+from cellcounter.main.views import NewCountTemplateView, CellImageListView, CellTypesListView, CellImageDetailView, similar_images, thumbnail
+
+admin.autodiscover()
 
 urlpatterns = [
-                  url(r'^$', views.NewCountTemplateView.as_view(), name="new_count"),
-                  url(r'^discover/', TemplateView.as_view(template_name="main/discover.html"), name="discover"),
-                  url(r'^about/', TemplateView.as_view(template_name="main/about.html"), name="about"),
-                  url(r'^help/', TemplateView.as_view(template_name="main/help.html"), name="help"),
-                  url(r'^images/celltype/(?P<cell_type>\w+)/', views.CellImageListView.as_view(),
-                      name="images_by_cell_type"),
-                  url(r'^images/similar/(?P<cell_image_pk>\d+)/', views.similar_images, name="images_by_similar_cell"),
-                  url(r'^images/thumbnail/(?P<cell_image_pk>\d+)/', views.thumbnail, name="thumbnail"),
-                  url(r'^images/page/(?P<cell_image_pk>\d+)/', views.CellImageDetailView.as_view(), name="page"),
-                  url(r'^api/cell_types/', views.CellTypesListView.as_view(), name="cell_types"),
-                  url(r'^api/stats/', include('cellcounter.statistics.urls')),
-                  url(r'^login/', LoginView.as_view(template_name="main/login.html"), name="login"),
-                  url(r'^logout/', LogoutView.as_view(next_page="/"), name='logout'),
-                  url(r'^api/keyboards/', include('cellcounter.cc_kapi.urls')),
-                  url(r'^accounts/', include('cellcounter.accounts.urls')),
-                  url(r'^admin/', admin.site.urls),
-                  url(r'^terms', TemplateView.as_view(template_name="main/terms.html"), name="terms"),
-                  url(r'^privacy', TemplateView.as_view(template_name="main/privacy.html"), name="privacy")
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                       url(r'^$', NewCountTemplateView.as_view(),
+                           name="new_count"),
+                       url(r'^discover/$', TemplateView.as_view(
+                           template_name="main/discover.html"), name="discover"),
+                       url(r'^about/$', TemplateView.as_view(
+                           template_name="main/about.html"), name="about"),
+                       url(r'^help/$', TemplateView.as_view(
+                           template_name="main/help.html"), name="help"),
+                       url(r'^images/celltype/(?P<cell_type>\w+)/$',
+                           CellImageListView.as_view(), name="images_by_cell_type"),
+                       url(r'^images/similar/(?P<cell_image_pk>\d+)/$',
+                           similar_images, name="images_by_similar_cell"),
+                       url(r'^images/thumbnail/(?P<cell_image_pk>\d+)/$',
+                           thumbnail, name="thumbnail"),
+                       url(r'^images/page/(?P<cell_image_pk>\d+)/$',
+                           CellImageDetailView.as_view(), name="page"),
+                       url(r'^api/cell_types/$', CellTypesListView.as_view(),
+                           name="cell_types"),
+                       url(r'^api/stats/$', include('cellcounter.statistics.urls')),
+                       url(r'^login/$', login,
+                           {'template_name': 'main/login.html'}, name='login'),
+                       url(r'^logout/$', logout, {'next_page': '/'},
+                           name='logout'),
+                       url(r'^api/keyboards/', include(
+                           'cellcounter.cc_kapi.urls')),
+                       url(r'^accounts/', include(
+                           'cellcounter.accounts.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^terms$', TemplateView.as_view(
+                           template_name="main/terms.html"),
+                           name="terms"),
+                       url(r'^privacy$', TemplateView.as_view(
+                           template_name="main/privacy.html"),
+                           name="privacy")
+]
+
+urlpatterns += staticfiles_urlpatterns()
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
