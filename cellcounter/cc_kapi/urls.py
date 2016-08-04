3dd2ebf12c8bfb10c4c95285748f4519cd6e8baa
JV Clemence <james+github@jvc26.org>
2016-08-04 10:44:25 +0100

    Make urls.py Django 1.10 compatible

--- cellcounter/cc_kapi/urls.py
+++ cellcounter/cc_kapi/urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .views import KeyboardsListCreateView, KeyboardGetUpdateDestroyView, DefaultKeyboardView
 
-urlpatterns = patterns('',
-                       url('^$', KeyboardsListCreateView.as_view(),
-                           name='keyboards'),
-                       url('^default/$', DefaultKeyboardView.as_view(),
-                           name='default-keyboard'),
-                       url('^(?P<keyboard_id>\d+)/$', KeyboardGetUpdateDestroyView.as_view(),
-                           name='keyboard-detail'),
-                       )
+urlpatterns = [
+    url('^$', KeyboardsListCreateView.as_view(), name='keyboards'),
+    url('^default/$', DefaultKeyboardView.as_view(), name='default-keyboard'),
+    url('^(?P<keyboard_id>\d+)/$', KeyboardGetUpdateDestroyView.as_view(), name='keyboard-detail'),
+]
