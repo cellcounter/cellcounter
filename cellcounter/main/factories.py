a2869ea18b06102755e9ff527fa821572dde2635
JV Clemence <james+github@jvc26.org>
2016-08-04 14:31:53 +0100

    Create new factories for all Models

--- cellcounter/main/factories.py
+++ cellcounter/main/factories.py
@@ -2,7 +2,7 @@ import factory
 import factory.fuzzy
 from django.contrib.auth.models import User
 
-from cellcounter.main.models import CellType
+from cellcounter.main import models
 
 
 class UserFactory(factory.django.DjangoModelFactory):
@@ -22,10 +22,71 @@ class UserFactory(factory.django.DjangoModelFactory):
 
 class CellTypeFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = CellType
+        model = models.CellType
 
     readable_name = 'test'
     machine_name = 'test'
     abbr_name = 'test'
     comment = "Test"
     visualisation_colour = '#FFFFFF'
+
+
+class LicenseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.License
+
+    title = "a"
+    details = "a"
+
+
+class CopyrightHolderFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.CopyrightHolder
+
+    name = "a"
+
+    @factory.post_generation
+    def users(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            for user in extracted:
+                self.user.add(user)
+        else:
+            self.user.add(UserFactory())
+
+
+class CellImageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.CellImage
+
+    title = "test"
+    description = "test"
+    file = factory.django.ImageField(color='blue')
+    thumbnail_left = 1
+    thumbnail_top = 1
+    thumbnail_width = 100
+
+    celltype = factory.SubFactory(CellTypeFactory)
+    uploader = factory.SubFactory(UserFactory)
+    copyright = factory.SubFactory(CopyrightHolderFactory)
+    license = factory.SubFactory(LicenseFactory)
+
+
+class SimilarLookingGroupFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.SimilarLookingGroup
+
+    name = "a"
+
+    @factory.post_generation
+    def cell_image(self, create, extracted):
+        if not create:
+            return
+
+        if extracted:
+            for cell_image in extracted:
+                self.cell_image.add(cell_image)
+        else:
+            self.cell_image.add(CellImageFactory())
