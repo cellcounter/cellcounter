03b1b92ed9843afc66d8c33a94baa7ef66a2f0e1
JV Clemence <james+github@jvc26.org>
2016-08-04 14:46:16 +0100

    Implement CountInstance __str__ methods compatible with Py2/3

--- cellcounter/statistics/models.py
+++ cellcounter/statistics/models.py
@@ -1,10 +1,15 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 
+@python_2_unicode_compatible
 class CountInstance(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     session_id = models.CharField(max_length=32)
     ip_address = models.GenericIPAddressField()
     user = models.ForeignKey(User, null=True, default=None)
     count_total = models.IntegerField()
+
+    def __str__(self):
+        return "CountInstance: {0} at {1}".format(self.session_id, self.timestamp)
