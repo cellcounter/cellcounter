70d3954dee6530995f977f7b0239b4b70a4a5038
Olly Madge <oghm2@srcf.ucam.org>
2019-07-09 16:13:05 +0100

    Javascript testing framework.

---
+++ test_integration.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export NODE_PATH=$VIRTUAL_ENV/lib/node_modules
+
+
+TMP_SERVER_LOG_FILE=$(tempfile)
+
+export DEBUG=True
+# disable autoreload or Django unhelpfully changes PID
+python -u manage.py runserver --noreload &>$TMP_SERVER_LOG_FILE &
+
+DEBUG_SERVER_PID=$!
+
+sleep 1
+
+while ! grep -m1 'Quit the server with CONTROL-C.' < $TMP_SERVER_LOG_FILE; do
+    sleep 1
+done
+
+mocha js_test/integration.js
+
+# kill the server process
+kill $DEBUG_SERVER_PID
+
+# delete the temporary log file
+rm $TMP_SERVER_LOG_FILE
+
